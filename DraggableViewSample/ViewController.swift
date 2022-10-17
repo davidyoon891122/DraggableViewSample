d14ad3ae9f7d02bb66a95764fbd2a306276ad5e2
Yoonjiwon <k112751@koreainvestment.com>
2022-10-17 11:37:20 +0900

    Add an open button
    * Added an open button

--- DraggableViewSample/ViewController.swift
+++ DraggableViewSample/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    private lazy var openViewButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Open", for: .normal)
+        button.setTitleColor(.label, for: .normal)
+        button.setTitleColor(.label.withAlphaComponent(0.3), for: .highlighted)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        
     }
 
 
+}
+
+private extension ViewController {
+    func setupViews() {
+        [
+            openViewButton
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+        
+        openViewButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
+    }
 }
 
